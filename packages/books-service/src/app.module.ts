fcad205be3f0da8bb2850bc5ba86c7aed602d993
dadazheng <lovepocky@foxmail.com>
2022-06-27 22:37:08 +0800

    feat(books): init books entity

--- packages/books-service/src/app.module.ts
+++ packages/books-service/src/app.module.ts
@@ -1,10 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      debug: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql')
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
